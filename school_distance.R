7d5134bb4df961eb03477416f2ca2638cc294ad9
restrellado <ryan.estrellado@gmail.com>
2018-06-26 10:00:54 -0700

    Switch to read_excel to read data in

--- school_distance.R
+++ school_distance.R
@@ -1,3 +1,27 @@
 library(tidyverse)
+library(readxl) 
+library(here)
 
-schools <- read_tsv("https://www.cde.ca.gov/schooldirectory/report?rid=dl1&tp=txt")
+#------------------------------------------------------------------------------
+
+raw_data <- read_excel(here("data", "pubschls.xlsx"))
+
+#------------------------------------------------------------------------------
+
+schools <- raw_data %>% 
+  # Remove all but Active 
+  # Merged records are missing location data 
+  filter(StatusType %in% c("Active")) %>% 
+  select(SOC, County, District, School, Longitude, Latitude)
+schools
+
+#------------------------------------------------------------------------------
+
+# Check for NAs 
+schools %>% map(~mean(is.na(.))) 
+
+#------------------------------------------------------------------------------
+
+# Look at missing long and lat 
+schools %>% 
+  filter(is.na(Longitude)) 
